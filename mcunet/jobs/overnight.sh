e8a5c9baef3d8a4890bb7ddbed4f5655cb4fa535
Michael Ruan <michael.ruan@mail.utoronto.ca>
2022-04-21 20:48:20 -0400

    diff archs

--- mcunet/jobs/overnight.sh
+++ mcunet/jobs/overnight.sh
@@ -5,10 +5,10 @@
 #python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 1 --lr 0.1
 
 python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.2
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 2 --lr 0.2
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 1 --lr 0.2
-
+python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.1
 python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.05
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 2 --lr 0.05
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 1 --lr 0.05
+
+python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.2
+python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.1
+python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --resolution 112 --seed 1000 --layers 3 --lr 0.05
 
--- mcunet/jobs/overnight_wang.sh
+++ mcunet/jobs/overnight_wang.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 1000 --lr 0.3
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 2000 --lr 0.3
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 3000 --lr 0.3
-
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 1000 --lr 0.2
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 2000 --lr 0.2
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 3000 --lr 0.2
-
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 1000 --lr 0.1
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 1000 --lr 0.05
-#python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-112" --seed 1000 --lr 0.01
-
 python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.1 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 2 --lr 0.1 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 1 --lr 0.1 --batch_size 16
-
 python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.2 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 2 --lr 0.2 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 1 --lr 0.2 --batch_size 16
-
 python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.05 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 2 --lr 0.05 --batch_size 16
-python train_kvasir.py --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 1 --lr 0.05 --batch_size 16
+
+python train_kvasir.py --arch resnet32 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.1 --batch_size 16
+python train_kvasir.py --arch resnet32 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.2 --batch_size 16
+python train_kvasir.py --arch resnet32 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.05 --batch_size 16
+
+python train_kvasir.py --arch resnet44 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.1 --batch_size 16
+python train_kvasir.py --arch resnet44 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.2 --batch_size 16
+python train_kvasir.py --arch resnet44 --data_dir "C:\Users\Michael Ruan\Documents\Github\tinyml\mcunet\data\kvasir-dataset-v2-cropped-224" --resolution 224 --seed 1000 --layers 3 --lr 0.05 --batch_size 16
 
